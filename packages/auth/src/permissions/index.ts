b4e8409432a8ceee6c99de7ade736a16ab5e141b
euviniciuss <dev.mvsc@gmail.com>
2026-05-14 00:37:18 -0300

    feat: criando permissões condicionais

--- packages/auth/src/permissions/index.ts
+++ packages/auth/src/permissions/index.ts
@@ -5,8 +5,10 @@ export const permissions: Record<TRole, TPermissionsByRole> = {
   ADMIN(_, { can }) {
     can("manage", "all")
   },
-  MEMBER(_, { can }) {
-    can("invite", "User")
+  MEMBER(user, { can }) {
+    can("delete", "Organization")
+    can(["create", "get"], "Project")
+    can(["update", "delete"], "Project", { ownerId: { $eq: user.id } })
   },
   BILLING() {},
 }
