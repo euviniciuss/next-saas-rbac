8f5162dd9cd4bc4a84d212acab09c5a6009a0647
euviniciuss <dev.mvsc@gmail.com>
2026-05-15 15:22:55 -0300

    feat: criando rota de criação de usuário

--- apps/api/src/http/routes/auth/create-account/index.ts
+++ apps/api/src/http/routes/auth/create-account/index.ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
 import type { FastifyInstance } from "fastify"
 import type { ZodTypeProvider } from "fastify-type-provider-zod"
 import { usersSchema } from "./schema"
@@ -10,8 +12,32 @@ export async function createAccount(app: FastifyInstance) {
         body: usersSchema,
       },
     },
-    () => {
-      return "User created"
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithEmail) {
+        return reply
+          .status(400)
+          .send({ message: "User with same email already exists." })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
