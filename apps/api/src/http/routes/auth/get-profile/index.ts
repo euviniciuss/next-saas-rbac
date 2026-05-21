9851d6c7fc3416df1a4f142633a95e62ebf0d415
euviniciuss <dev.mvsc@gmail.com>
2026-05-21 16:48:54 -0300

    feat: adicionando Authorization no swagger

--- apps/api/src/http/routes/auth/get-profile/index.ts
+++ apps/api/src/http/routes/auth/get-profile/index.ts
@@ -15,7 +15,7 @@ export async function getProfile(app: FastifyInstance) {
         schema: {
           tags: ["Auth"],
           summary: "Get authenticate user profile",
-
+          security: [{ bearerAuth: [] }],
           response: {
             201: reponseSucessSchema,
           },
